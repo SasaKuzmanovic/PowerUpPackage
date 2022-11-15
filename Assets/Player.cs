10927e93420d08303a7daa89c798e579a8463587
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-11-15 11:59:00 +0000

    Fixed speed powerup

---
+++ Assets/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+
+    public bool collided = false;
+
+    public GameObject pickup;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (collided)
+        {
+            CallPackage(pickup);
+        }
+    }
+
+    void CallPackage(GameObject t_pickup)
+    {
+        if (speed > 5.0f)
+        {
+            speed = t_pickup.GetComponent<Speed_Script>().DecaySpeedOverTime(speed);
+            Debug.Log(speed);
+        }       
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            pickup = collision.gameObject;
+            pickup.gameObject.transform.position = new Vector3(3000, 3000, 3000);
+            speed = collision.gameObject.GetComponent<Speed_Script>().addSpeedToPlayer(speed);
+            collided = true;            
+        }
+    }
+
+
+}
--- Packages/ie.setu.powerups/Runtime/Speed_Script.cs
+++ Packages/ie.setu.powerups/Runtime/Speed_Script.cs
@@ -6,69 +6,31 @@ public class Speed_Script : MonoBehaviour
 {
     public GameObject player;
     public float speed;
-    public float timer = 0.0f;
-    public float timerEnd = 5.0f; // Define how long you want the timer to be
-    public bool timeStart = false;
     public bool spawned = false;
 
-    private void Update()
+
+    public float addSpeedToPlayer(float t_speed)
     {
-        if (timeStart)
-        {
-            speedTimer();
-            DecaySpeedOverTime();
-        }
+        speed = t_speed * 1.2f;
+
+        return speed;
     }
 
-
-    public void addSpeedToPlayer()
+    public float DecaySpeedOverTime(float t_speed)
     {
-        player.GetComponent<Player>().speed *= 1.2f;
-        speed = player.GetComponent<Player>().speed;
-    }
-
-    public void speedTimer()
-    {
-        if (timer < timerEnd)
+        if (speed > 5.0f)
         {
-            timer += Time.deltaTime;
-        }
-        else
-        {
-            timeStart = false;
-            timer = 0.0f;
-        }
-        
+            t_speed += (-0.22f * Time.deltaTime);
 
-    }
-
-    public void DecaySpeedOverTime()
-    {
-        if (timer < timerEnd && speed > 5.0f)
-        {
-            speed = player.GetComponent<Player>().speed;
-
-            speed += (-0.22f * Time.deltaTime);
-            player.GetComponent<Player>().speed = speed;
+            return t_speed;
         }
+        return t_speed;
     }
 
     public void SpawnPickup()
     {
         if (!spawned)
             Instantiate(this); spawned = true;
-
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("I Ded");
-            transform.position = new Vector3(3000, 3000, 3000);
-            addSpeedToPlayer();
-            timeStart = true;
-        }
-    }
+    }  
 
 }
